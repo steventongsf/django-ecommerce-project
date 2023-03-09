306e0040fa981822f6dca68a9c95e35038e9edc8
Steven Tong <steventongsf@gmail.com>
2023-03-09 12:59:38 -0800

    cart view update

--- store/views.py
+++ store/views.py
@@ -1,4 +1,8 @@
+from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
+from apparelstore import settings
+from carts.models import CartItem
+from carts.views import _cart_id
 
 from category.models import Category
 
@@ -33,14 +37,17 @@ def product_detail(request, category_slug, product_slug):
         # VariationManager not working so explicitly getting variations and passing to context
         product_colors = Variation.objects.filter(product_id=single_product.id,variation_category="color")
         product_sizes = Variation.objects.filter(product_id=single_product.id,variation_category="size")
-        print("product_colors: ",product_colors)
+        is_in_cart = CartItem.objects.filter(cart__cart_id=_cart_id(request), product=single_product).exists()
         for s in product_sizes:
             print("product_size: ",s.variation_value)
+        # if settings.DEBUG:
+        #     return HttpResponse("Is in cart: "+str(is_in_cart))
     except Exception as e:
         raise e
     context = {
         'single_product': single_product,
         'product_colors':product_colors,
         'product_sizes':product_sizes,
+        'is_in_cart': is_in_cart,
     }
     return render(request, "store/product-detail.html", context)
